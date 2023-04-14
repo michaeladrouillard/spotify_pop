59f531f85454f70828a80e5d5429d28a2e9510a5
michaeladrouillard <droumich99@gmail.com>
2023-04-14 17:07:17 -0400

    added notes 2 self

--- scripts/09-rohan_messing_around.R
+++ scripts/09-rohan_messing_around.R
@@ -91,6 +91,8 @@ glmnet_spec <-
 # install.packages("themis")
 library(themis)
 
+
+#so this is comparing the basic model and the downsampled one
 wf_set <-
   workflow_set(
     list(basic = is_jack,
@@ -108,6 +110,19 @@ doParallel::registerDoParallel()
 
 set.seed(853)
 
+#first u pass in ur wf set, then u pass which function,
+#then u pass in things u need for the tuning. like resamples,
+# values for penalty (grid=16), pass in metrics
+#accuracy and roc_auc are basic metrics
+# add logloss, which looks at model as a whole, measures how wrong u r.
+# helps measure that u do a better job. i.e. .6 and .9 would
+#still be in positive class, but it tells you how wrong you are
+#sensitivity, specifity: measure how well u do on one class versus the other
+# (important for unbalanced dataset)
+#passing params. knows from exp that default penalty is really broad -10, 0.
+#make it much much narrower
+
+
 tune_rs <- 
   workflow_map(
     wf_set,
@@ -122,13 +137,50 @@ tune_rs
 
 autoplot(tune_rs) + theme(legend.position = "none")
 
+#x axis is workflow rank, so which workflow are we on
+#ok so we have good accuracy and good specificity. but low
+#low specificity and low log loss
+#dont quite understand workflow rank?
+#theres no right metric to evaluate model. you have to understand
+# it holistically and look at a bunch of different metrics
+#sanity check with rohan
+
 rank_results(tune_rs, rank_metric = "sensitivity")
+rank_results(tune_rs, rank_metric = "mn_log_loss")
+rank_results(tune_rs, rank_metric = "accuracy")
+rank_results(tune_rs, rank_metric = "specificity")
+
+#the only one where downsampling is ranked higher is specificity...
+#wouldn't it make more sense to go with the basic one then?
 
 downsample_rs <-
   tune_rs %>%
   extract_workflow_set_result("downsampling_glmnet")
 
-autoplot(downsample_rs)
+#tuning result for recipe that included downsampling + glmnet model
+plotdownsample <- autoplot(downsample_rs) + ggtitle("Downsampled Data")
+
+
+basic_rs <-
+  tune_rs %>%
+  extract_workflow_set_result("basic_glmnet")
+
+plotbasic <- autoplot(basic_rs) + ggtitle("Original Data")
+
+library(gridExtra)
+grid.arrange(plotdownsample, plotbasic, ncol=2)
+#on the right is a lot of regularization, so just throw a bunch of predictors out
+
+#we want specificity because we specifically care about the variables..
+
+
+
+
+#this gives me this penalty. this is the biggest penalty
+#where u get log loss that is about the same performance has the best value
+
+#should i change this to specificity?
+
 
 best_penalty <- 
   downsample_rs %>%
@@ -136,6 +188,9 @@ best_penalty <-
 
 best_penalty
 
+
+# last fit is a convenient helper function that fits one time to the training data
+#and evaluates one time on the testing data
 final_fit <-  
   wf_set %>% 
   extract_workflow("downsampling_glmnet") %>%
@@ -145,6 +200,7 @@ final_fit <-
 final_fit
 
 collect_metrics(final_fit)
+
 
 collect_predictions(final_fit) %>%
   conf_mat(jack, .pred_class)
